d9a44246d20ec992d99430abf4ae85a39915c56a
Omair Jaswal <OmairAJ@users.noreply.github.com>
2025-09-07 03:46:59 +0500

    Added student detail prints and implements addition functionality

--- Q2_AI-201_Fundamentals_of_Agentic_AI/assignment_2/uv-assignment/simple-app/main.py
+++ Q2_AI-201_Fundamentals_of_Agentic_AI/assignment_2/uv-assignment/simple-app/main.py
@@ -1,5 +1,16 @@
+def add(a, b):
+    print(f"{a} + {b} = {a + b}\n")
+
+
+def prints():
+    print("Student Name:\tOmair Abid Jaswal")
+    print("Roll Number:\tPIAIC115285\n")
+    add(2, 2)
+    
+    
 def main():
-    print("Hello from simple-app!")
+    print("\nHello from simple-app!\n")
+    prints()
 
 
 if __name__ == "__main__":
