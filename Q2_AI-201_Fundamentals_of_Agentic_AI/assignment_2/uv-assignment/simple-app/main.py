30d1db7cc93c596b21a5460c7cb2c8550ce7ba46
Omair Jaswal <OmairAJ@users.noreply.github.com>
2025-09-07 03:59:10 +0500

    Added helpful code comments

--- Q2_AI-201_Fundamentals_of_Agentic_AI/assignment_2/uv-assignment/simple-app/main.py
+++ Q2_AI-201_Fundamentals_of_Agentic_AI/assignment_2/uv-assignment/simple-app/main.py
@@ -1,13 +1,17 @@
+# Functions
+# Simple function for addition of two numbers
 def add(a, b):
     print(f"{a} + {b} = {a + b}\n")
 
-
+# Screen outputs
 def prints():
     print("Student Name:\tOmair Abid Jaswal")
     print("Roll Number:\tPIAIC115285\n")
+    
     add(2, 2)
-    
-    
+
+
+# Minimal main program
 def main():
     print("\nHello from simple-app!\n")
     prints()
