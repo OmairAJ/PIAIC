cd3b3d3dc6e3172f1f5ddcb8b10fde5e2609e5ff
Omair Jaswal <OmairAJ@users.noreply.github.com>
2025-09-07 05:01:10 +0500

    Added student detail prints and implements addition functionality in main module

--- Q2_AI-201_Fundamentals_of_Agentic_AI/assignment_2/uv-assignment/packaged-app/src/packaged_app/main.py
+++ Q2_AI-201_Fundamentals_of_Agentic_AI/assignment_2/uv-assignment/packaged-app/src/packaged_app/main.py
@@ -1,3 +1,17 @@
+# Functions
+# Simple function for addition of two numbers
+def add(a, b) -> str:
+    print(f"{a} + {b} = {a + b}\n")
+
+# Screen outputs
+def prints() -> None:
+    print("Student Name:\tOmair Abid Jaswal")
+    print("Roll Number:\tPIAIC115285\n")
+    
+    add(2, 2)
+
+
+# Minimal main program
 def main() -> None:
     print("\nHello from packaged-app-module!\n")
     prints()
