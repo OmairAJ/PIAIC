fe12fa0547dceb99f69724a167e76a4e02ad581b
Omair Jaswal <OmairAJ@users.noreply.github.com>
2025-10-26 12:27:00 +0500

    Q2 Assignment 4

---
+++ Q2_AI-201_Fundamentals_of_Agentic_AI/assignment_4/hello-agent-assignment/packaged-agent/src/packaged_agent/main.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from agents import Agent, Runner, AsyncOpenAI, set_default_openai_client, OpenAIChatCompletionsModel, set_tracing_disabled
+
+load_dotenv()
+
+# Store credentials (REMOVE before committing)
+api_key = os.getenv("GEMINI_API_KEY")
+model = "gemini-2.5-flash-lite"
+
+# Configure Gemini as the external client
+external_client = AsyncOpenAI(
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+    api_key=api_key,
+)
+set_default_openai_client(external_client)
+
+# Register the model (Chat Completions) and disable tracing (not supported for Gemini)
+llm_model = OpenAIChatCompletionsModel(
+    model=model,
+    openai_client=external_client,
+)
+set_tracing_disabled(True)
+
+# Define your agent
+agent = Agent(
+    name="simple_agent",
+    instructions="You are a helpful assistant, specialized in providing single-line responses.",
+    model=llm_model
+)
+
+# Wrap execution in a function (packaged app style)
+def run_agent():
+    response = Runner.run_sync(agent, "What is AI?")
+    print(response.final_output)
